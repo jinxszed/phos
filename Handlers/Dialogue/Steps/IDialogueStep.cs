bde926cbc5ae42c33d40ccc45c0ab45a0b06224e
phos <phos@PHOSPHOPHYLLITE>
2023-06-08 02:32:23 -0700

    base dialogue handlers created

--- Handlers/Dialogue/Steps/IDialogueStep.cs
+++ Handlers/Dialogue/Steps/IDialogueStep.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace phos.Handlers.Dialogue
+namespace phos.Handlers.Dialogue.Steps
 {
     public interface IDialogueStep
     {
